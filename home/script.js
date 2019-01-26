ae256781d0fb40e98be93b1339e01f51ab984cf0
SarahHennessy17 <42851976+SarahHennessy17@users.noreply.github.com>
2019-01-26 15:04:13 -0500

    Add files via upload

--- home/script.js
+++ home/script.js
@@ -1,17 +1,38 @@
 $( document).ready(function(){
 
-	console.log("testing");
-
+	console.log("testing testing 1 2 3");
 
 $("h1").mouseenter(function(e){
-  // $(".bio").removeClass("hide");
   $('.bio').css('opacity',1);
 });
 
 $("h1").mouseout(function(e){
-	// $(".bio").addClass("hide");
     $('.bio').css('opacity',0);
-	});
+});
+
+$(".link1").mouseenter(function(e){
+  $('.movie').css('opacity',1);
+});
+
+$(".link1").mouseout(function(e){
+    $('.movie').css('opacity',0);
+});
+
+$(".link2").mouseenter(function(e){
+  $('.site').css('opacity',1);
+});
+
+$(".link2").mouseout(function(e){
+    $('.site').css('opacity',0);
+});
+
+$(".link3").mouseenter(function(e){
+  $('.game').css('opacity',1);
+});
+
+$(".link3").mouseout(function(e){
+    $('.game').css('opacity',0);
+});
 
 
 });
