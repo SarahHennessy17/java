59c9c7d5d2536dfc926221176c1f26ce54fb4ac7
SarahHennessy17 <42851976+SarahHennessy17@users.noreply.github.com>
2019-02-28 18:28:43 -0500

    Add files via upload

---
+++ tip/dish/js/script.js
@@ -0,0 +1,168 @@
+$( document).ready(function(){
+
+	var peopleCounter = 2;
+	var emojiCounter = 2;
+	var emojiMood = ["../emojis/lick.png","../emojis/love.png","../emojis/laugh.png","../emojis/cry.png","../emojis/gross.png","../emojis/fear.png","../emojis/sleep.png","../emojis/adventure.png","../emojis/think.png","../emojis/big.png","../emojis/dark.png","../emojis/family.png","../emojis/alien.png","../emojis/cool.png","../emojis/cold.png"];
+	const emojiLength = emojiMood.length;
+	var myPrice = $('.myPrice');
+
+//Whenever we click + attach more people
+$('#add').click(function(){ 
+	//pick a Face
+	var url = emojiMood[emojiCounter];  
+ 	//attach more people
+	$('#people').append("<img src=" + url + " class='face'>");
+	addDishes();	
+	//Scroll with emojis
+   	$(document).scrollTop($(document).height());
+	//Now you can remove people
+	$('#minus').removeClass("hide");
+	//And Add to our people count
+	peopleCounter++;
+	emojiCounter++;
+	//Tell us how many people we've got
+	$('#party').html("Party Size: " + peopleCounter)
+	//reset our face count when we run out of faces
+	if(emojiCounter > emojiLength - 1){
+		emojiCounter = 0;
+	}
+})
+
+//Add more people
+function addDishes(){
+	$('.face').last().after("<input type='text' class='myPrice hide' placeholder='$'><button class='addMore hide none'>+</button>");
+}
+
+//Add more money boxes
+// $('.addMore').click(function(){ 
+// 	$(this).after("<input type='text' class='myPrice bonus'><button class='addMore bonus'>+</button>");
+// 	console.log("heloooo");
+// })
+
+//Whenever we click - remove people
+$('#minus').click(function(){   
+	//remove a person but THIS IS REMOVING ALL OF THEM VISUALLY
+	// $(".person").remove();
+	$('.face').last().remove();
+	//And Subtract from our people count
+	peopleCounter--;
+	emojiCounter--;
+	//Tell us how many people we've got
+	$('#party').html("Party Size: " + peopleCounter)
+	//If you only have two people you can't delete any
+	if(peopleCounter == 2){
+		$('#minus').addClass("hide");
+	}
+})
+
+//When we click bill we finalize people count
+$('#party').click(function(){     
+	//No more adding people
+	$('#add').addClass("hide");		
+	//No more remove people
+	$('#minus').addClass("hide");
+	//No more changing party size
+	$('#party').addClass("none");
+	//Let's add prices
+	$('.myPrice').removeClass("hide");
+	$('#prices').removeClass("none");	
+	//Let's add MORE prices
+	// $('.addMore').removeClass("hide");	
+})
+
+$('#prices').click(function(){ 
+	//THE PRICE INPUT
+	$('#input').removeClass("hide");
+	//THE PRICE INPUT
+	$('#prices').addClass("none");
+	//No more changing prices
+	$('.myPrice').attr('readonly', true);	
+	getTax();
+})
+
+//Suggest a Tip
+$('#suggestTip').click(function(){ 
+	tipGive();
+})
+
+//equal sign gets us total
+$('#splitIt').click(function(){ 
+	splitTime();
+})
+
+function getTax(){
+	//get the bill total
+	var billPrice = 0;
+    $(".myPrice").each(function(){
+        billPrice += +$(this).val();
+    });
+	//get the tax
+	var tax = billPrice * .08875 ;
+	var suggestTax = Math.round(tax);
+	//suggest the tax
+	$('#taxPrice').val(suggestTax);
+}
+
+function tipGive(){
+	//Get the tax
+	var taxAmount = Number($('#taxPrice').val());
+	//Get the total
+	var billPrice = Number($('.myPrice').val());
+	//add tax and tip
+	var ourTotal = billPrice + taxAmount;
+	//calculate the tip
+	var tip = ourTotal * .18;
+	// Round Tip
+	var totalTip = Math.round(tip);
+	//Show Tip Section
+	$('#tip').removeClass("hide");
+	//suggest the tip
+	$('#tipPrice').val(totalTip)
+	//Make room for the tip
+	$("#bill").css("height", "15vh");
+}
+
+function splitTime(){
+	//Make room for total
+	$("#bill").css("height", "22vh");
+	//Show Total
+	$('.total').removeClass("hide");
+	//Get the tax + tip Price
+	var taxPrice = Number($('#taxPrice').val());
+	var tipPrice = Number($('#tipPrice').val());
+	//Split the tax/tip
+	var splitAmount = tipPrice + taxPrice;
+	var splitIt = splitAmount/peopleCounter;
+
+	//how much does everyone need to add to their total
+	$('.total').val(splitIt);
+	$('.total').text("Everybody add $" + splitIt + " to your total");
+}
+
+//Calculate the equation when we press the keyboard
+$('#input').keydown(function(){   
+
+  var keynum = event.keyCode;
+
+  if(keynum == 13) {
+   tipGive();
+  } 
+
+})
+
+//Calculate the equation when we press the keyboard
+$('#tipPrice').keydown(function(){   
+
+  var keynum = event.keyCode;
+
+  if(keynum == 13) {
+   splitTime();
+  } 
+
+})
+
+});
+
+
+
+
--- tip/js/script.js
+++ tip/js/script.js
@@ -5,12 +5,20 @@ $( document).ready(function(){
 	var emojiMood = ["emojis/lick.png","emojis/love.png","emojis/laugh.png","emojis/cry.png","emojis/gross.png","emojis/fear.png","emojis/sleep.png","emojis/adventure.png","emojis/think.png","emojis/big.png","emojis/dark.png","emojis/family.png","emojis/alien.png","emojis/cool.png","emojis/cold.png"];
 	const emojiLength = emojiMood.length;
 
+//If we choose to split the check evenly
+$('.split').click(function(){    
+	//Take away our options
+	$('#option').addClass("hide");		
+	//Take away our backdrop
+	$('.back').addClass("hide");
+})
+
 //Whenever we click + attach more people
 $('#add').click(function(){ 
 	//pick a Face
 	var url = emojiMood[emojiCounter];  
  	//attach more people
-	$('#people').append("<img src=" + url + " class='face'>");	
+	$('#people').append("<img src=" + url + " class='face'>");
 	//Scroll with emojis
    	$(document).scrollTop($(document).height());
 	//Now you can remove people
@@ -56,6 +64,10 @@ $('#party').click(function(){
 
 //Suggest a Tip
 $('#suggestTip').click(function(){ 
+	tipGive();
+})
+
+function tipGive(){
 	//Get the Bill Price
 	var billPrice = $('#billPrice').val();
 	//calculate the tip
@@ -68,10 +80,14 @@ $('#suggestTip').click(function(){
 	$('#tipPrice').val(totalTip)
 	//Make room for the tip
 	$("#bill").css("height", "15vh");
-})
+}
 
 //When we click bill we finalize people count
 $('#splitIt').click(function(){ 
+	splitTime();
+})
+
+function splitTime(){
 	//Show Total
 	$('.total').removeClass("hide");
 	//Get the Bill + tip Price
@@ -86,17 +102,23 @@ $('#splitIt').click(function(){
 	//Make room for total
 	$("#bill").css("height", "22vh");
 	//Tell us about it
-	$('.total').text("Everybody Pay: $" + totalEach)
+	$('.total').text("Everybody Pay: $" + totalEach);
 	
+}
+
+//Calculate the equation when we press the keyboard
+$('#bill').keydown(function(){   
+
+  var keynum = event.keyCode;
+
+  if(keynum == 13) {
+   tipGive();
+   splitTime();
+  } 
+  
 })
 
 
 
+
 });
-
-
-
-
-
-
-
